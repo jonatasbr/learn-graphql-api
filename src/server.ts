1681dfd0f9894c85112a526a6529f7ba5c0c7f6d
Jonatas Braz <jonatasbraz@gmail.com>
2021-02-26 01:50:27 -0300

    configurando prettier, editorconfig e eslint

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-import express, { request, response } from 'express';
+import express from 'express';
 
 const app = express();
 
 app.get('/', (request, response) => {
   console.log('teste');
-  
+
   return response.json({ message: 'teste' });
-})
+});
 
 app.listen(4000, () => {
   console.log('Server started on port 4000');
-})
-
+});
